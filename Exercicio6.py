d955a1c6095e55de1080e145e45d76ebe29ad991
stephanie03m <53584967+stephanie03m@users.noreply.github.com>
2019-08-14 16:25:56 -0300

    Update Exercicio6.py

--- Exercicio6.py
+++ Exercicio6.py
@@ -1,1 +1,22 @@
-#Arquivo criado pelo colega FerPoletto.
+"""6 - Faça um algoritmo que leia um nº inteiro e mostre uma
+mensagem indicando se este número é par ou ímpar, e se é positivo
+ou negativo."""
+
+while True:
+    try:
+        numero = int(input("Digite um número inteiro: "))
+        break
+
+    except ValueError:
+        print("Digite número inteiro")
+
+if numero>=0:
+    print("{} número positivo".format(numero))
+
+else:
+    print("{} negativo".format(numero))
+
+if numero%2==0:
+    print("Par")
+else:
+    print("Impar")
