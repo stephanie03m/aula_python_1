974884c8e81e6aaa02b93e0e802f9af91c72a862
stephanie03m <53584967+stephanie03m@users.noreply.github.com>
2019-09-05 19:53:30 -0300

    Update Exercicio10.py

--- Exercicio10.py
+++ Exercicio10.py
@@ -1,2 +1,13 @@
 """10 - receba três notas de um aluno e
 informe se ele passou ou reprovou."""
+
+notas = []
+
+for i in range (3):
+    notas.append(float(input("Digite a nota do aluno: ")))
+
+if sum(notas)/3 >= 7:
+    print("A média do aluno é: {:.2f}. Aprovado".format(sum(notas)/3))
+
+else:
+    print("A média do aluno é: {:.2f}. Reprovado".format(sum(notas) / 3))
