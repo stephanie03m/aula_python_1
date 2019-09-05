278ebd9eaaa66ffb688e6a686473817c7bc37095
stephanie03m <53584967+stephanie03m@users.noreply.github.com>
2019-09-05 20:05:29 -0300

    Create Exercicio11.py

---
+++ Exercicio11.py
@@ -0,0 +1,6 @@
+"""11 -  Escreva um algoritmo para ler um valor
+(do teclado) e escrever (na tela) o seu antecessor."""
+
+numero = int(input("Digite um número: "))
+
+print("Número antecesor: {}".format(numero-1))
