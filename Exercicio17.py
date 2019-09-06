2df8212c11d5b928f01b92bf9d69390a13d3e4ad
stephanie03m <53584967+stephanie03m@users.noreply.github.com>
2019-09-05 21:29:04 -0300

    Create Exercicio17.py

---
+++ Exercicio17.py
@@ -0,0 +1,11 @@
+"""17 - Cria uma lista para armazenar 5 nomes fixos. Após inserir
+os 5 nome da lista mostre-os no console (utilize um for)."""
+
+nomes = []
+
+for i in range(5):
+    nomes.append(input("Digite um nome: "))
+
+for i in  nomes:
+    print(i)
+    
